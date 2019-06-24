aa9bf80cbcf717d3c91f16f6962f698ae6c9b1bc
Alexander Mancevice <amancevice@cargometrics.com>
2019-06-24 06:17:18 -0400

    break out auth/post classes

---
+++ lib/brutalismbot/post.rb
@@ -0,0 +1,55 @@
+module Brutalismbot
+  class Post < Hash
+    def created_after(time:)
+      created_utc.to_i > time.to_i
+    end
+
+    def created_utc
+      Time.at(dig("data", "created_utc").to_i).utc
+    end
+
+    def permalink
+      dig "data", "permalink"
+    end
+
+    def title
+      dig "data", "title"
+    end
+
+    def to_slack
+      {
+        blocks: [
+          {
+            type: "image",
+            title: {
+              type: "plain_text",
+              text: "/r/brutalism",
+              emoji: true,
+            },
+            image_url: url,
+            alt_text: title,
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text: "<https://reddit.com#{permalink}|#{title}>",
+              },
+            ],
+          },
+        ],
+      }
+    end
+
+    def url
+      images = dig "data", "preview", "images"
+      source = images.map{|x| x["source"] }.compact.max do |a,b|
+        a.slice("width", "height").values <=> b.slice("width", "height").values
+      end
+      CGI.unescapeHTML source.dig("url")
+    rescue NoMethodError
+      dig("data", "media_metadata")&.values&.first&.dig("s", "u")
+    end
+  end
+end
---
+++ spec/auth_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe Brutalismbot::Auth do
+
+  auth = Brutalismbot::Auth[JSON.parse({
+    ok:           true,
+    access_token: "<token>",
+    scope:        "identify,incoming-webhook",
+    user_id:      "UABCD1234",
+    team_name:    "My Slack Workspace",
+    team_id:      "TABCD1234",
+    incoming_webhook: {
+      channel:           "#brutalism",
+      channel_id:        "CABCD1234",
+      configuration_url: "https://workspace.slack.com/services/BABCD1234",
+      url:               "https://hooks.slack.com/services/TABCD1234/BABCD1234/1234567890abcdef12345678",
+    },
+    scopes: [
+      "identify",
+      "incoming-webhook",
+    ],
+  }.to_json)]
+
+  it "reads the channel_id" do
+    expect(auth.channel_id).to eq("CABCD1234")
+  end
+
+  it "reads the team_id" do
+    expect(auth.team_id).to eq("TABCD1234")
+  end
+
+  it "reads the webhook_url" do
+    expect(auth.webhook_url).to eq("https://hooks.slack.com/services/TABCD1234/BABCD1234/1234567890abcdef12345678")
+  end
+
+  it "posts the http body" do
+    expect_any_instance_of(Net::HTTP).to receive(:request).and_return('OK')
+    expect(auth.post(body: {fizz: "buzz"}.to_json)).to eq('OK')
+  end
+
+  it "posts the http body [DRYRUN]" do
+    expect(auth.post(body: {fizz: "buzz"}.to_json, dryrun: true)).to eq(true)
+  end
+end
