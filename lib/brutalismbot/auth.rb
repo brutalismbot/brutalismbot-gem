f2a40b3d8e79b6fedbe7330656490398fcfe23ef
Alexander Mancevice <amancevice@cargometrics.com>
2019-07-04 12:13:44 -0400

    remove #post from Auth

--- lib/brutalismbot/auth.rb
+++ lib/brutalismbot/auth.rb
@@ -4,21 +4,6 @@ module Brutalismbot
       dig "incoming_webhook", "channel_id"
     end
 
-    def post(body:, dryrun:nil)
-      uri = URI.parse webhook_url
-      ssl = uri.scheme == "https"
-      Net::HTTP.start(uri.host, uri.port, use_ssl: ssl) do |http|
-        if dryrun
-          Brutalismbot.logger.info "POST DRYRUN #{uri}"
-        else
-          Brutalismbot.logger.info "POST #{uri}"
-          req = Net::HTTP::Post.new uri, "content-type" => "application/json"
-          req.body = body
-          http.request req
-        end
-      end
-    end
-
     def team_id
       dig "team_id"
     end
