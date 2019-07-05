1217f659a50a129183b2d92023c8bde2db8c34a2
Alexander Mancevice <amancevice@cargometrics.com>
2019-07-05 08:01:40 -0400

    Post line size fixes

--- lib/brutalismbot/post.rb
+++ lib/brutalismbot/post.rb
@@ -66,19 +66,22 @@ module Brutalismbot
 
     class << self
       def stub
-        created_utc = Time.now.utc - rand(86400) - 86400
+        created_utc  = Time.now.utc - rand(86400) - 86400
+        post_id      = SecureRandom.alphanumeric(6).downcase
+        permalink_id = SecureRandom.alphanumeric.downcase
+        image_id     = SecureRandom.alphanumeric
         Post[{
           "kind" => "t3",
           "data" => {
-            "id"          => SecureRandom.alphanumeric(6).downcase,
+            "id"          => post_id,
             "created_utc" => created_utc.to_i,
-            "permalink"   => "/r/brutalism/comments/#{SecureRandom.alphanumeric}/test/",
+            "permalink"   => "/r/brutalism/comments/#{permalink_id}/test/",
             "title"       => "Post to /r/brutalism",
             "preview" => {
               "images" => [
                 {
                   "source" => {
-                    "url" => "https://preview.redd.it/#{SecureRandom.alphanumeric}.jpg",
+                    "url" => "https://preview.redd.it/#{image_id}.jpg",
                   }
                 }
               ]
