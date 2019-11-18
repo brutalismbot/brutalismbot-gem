357b37e3c3642e4ee19f65770a44f0cde552fdab
Alexander Mancevice <amancevice@cargometrics.com>
2019-11-18 16:47:39 -0500

    Update Brutalismbot::Twitter module

--- lib/brutalismbot/twitter/client.rb
+++ lib/brutalismbot/twitter/client.rb
@@ -1,21 +1,43 @@
+require "open-uri"
+require "tempfile"
+
 require "twitter"
+
+require "brutalismbot/version"
 
 module Brutalismbot
   module Twitter
+    module Environment
+      def env
+        client = ::Twitter::REST::Client.new do |config|
+          config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+          config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+          config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+          config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+        end
+        new(client: client)
+      end
+    end
+
     class Client
+      extend Environment
+
+      attr_reader :client
+
       def initialize(client:nil)
-        @client = client || Twitter::REST::Client.new
+        @client = client || ::Twitter::REST::Client.new
       end
 
-      class << self
-        def env
-          client = ::Twitter::REST::Client.new do |config|
-            config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-            config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-            config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-            config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-          end
-          new(client: client)
+      def push(post)
+        text = [post.title, post.permalink].join("\n")
+        file = Tempfile.new
+        begin
+          open(post.url){|res| file.write(res.read) }
+          file.rewind
+          @client.update_with_media(text, file)
+        ensure
+          file.close
+          file.unlink
         end
       end
     end
