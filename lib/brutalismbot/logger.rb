8e4f0b72ad9e31c856dca6f4aa75e4b0386e9f8e
Alexander Mancevice <alexander.mancevice@hey.com>
2021-01-03 14:32:56 -0500

    logger fix

--- lib/brutalismbot/logger.rb
+++ lib/brutalismbot/logger.rb
@@ -3,12 +3,11 @@ require "logger"
 module Brutalismbot
   module Logger
     def logger
-      # @@logger ||= ::Logger.new(File::NULL)
-      @@logger ||= ::Logger.new(STDOUT, formatter: -> (*x) { "#{x.last}\n" })
+      @logger ||= ::Logger.new($stderr, formatter: -> (*x) { "#{x.last}\n" })
     end
 
     def logger=(logger)
-      @@logger = logger
+      @logger = logger
     end
   end
 
